1f76ce2113fda7d7dc0ba23dac5b847d590803fd
chaar99 <carmenlopezcalvo.4c@gmail.com>
2020-12-01 17:28:03 +0100

    arregloo de detalle de producto

--- src/components/Detalle/detalle.jsx
+++ src/components/Detalle/detalle.jsx
@@ -9,11 +9,13 @@ class Detalle extends Component {
         const { elemento } = this.props;
         return (
             <div>
-                <img style={{ height: 175, width:200 }} src={"./img/"+ elemento.ruta} />
+                <p>fuwegfgwegfruwegf</p>
+                {elemento}
+                {/* <img style={{ height: 175, width:200 }} src={"./img/"+ elemento.ruta} />
                 <h2>{elemento.nombre}</h2>
                 <p>{elemento.descripcion}</p>
                 <p>{elemento.precio}€ / Unidad</p>
-                {elemento.stock >= 5 && <p>{"Solo quedan "+ elemento.stock + " en stock."} </p> }
+                {elemento.stock >= 5 && <p>{"Solo quedan "+ elemento.stock + " en stock."} </p> } */}
             </div>
         );
     }
--- src/components/Producto/producto.jsx
+++ src/components/Producto/producto.jsx
@@ -1,7 +1,5 @@
 import { Component } from 'react';
-import Detalle from '../Detalle/detalle';
-
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 class Producto extends Component {
     constructor(props) {
@@ -9,15 +7,12 @@ class Producto extends Component {
        
     }
 
-    detalle(ev, elemento) {
-        <Link to="/detalle" elemento={elemento} className="btn btn-default">Detalle</Link>
-    }
     render() {
         const { addProductCart, elemento } = this.props;
         return (            
             <div className="col-12 col-sm-3 p-3">
                 <p className="text-center">{elemento.id_productos}</p>
-                <img className="mx-auto d-block" style={{ height: 175, width:200 }} src={"./img/"+ elemento.ruta} onClick={(ev) => this.detalle(ev, elemento)}/>
+                <Link to="/detalle" elemento={elemento} className="btn btn-default"><img className="mx-auto d-block" style={{ height: 175, width:200 }} src={"./img/"+ elemento.ruta}/></Link>
                 <p className="text-center">{elemento.nombre}</p>
                 <p className="text-center small">{elemento.precio}€ / Unidad</p>
                 <button className="btn btn-primary w-100" onClick={() => addProductCart(elemento.id_productos, elemento.nombre)}>Añadir al carrito</button>
